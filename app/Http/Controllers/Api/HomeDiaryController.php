cce238b869b1b1c9365df187a3e80b752545a794
1292460369@qq.com <a310553455>
2020-07-05 19:23:37 +0800

    添加点赞， 添加不知道的什么东西，很多吧

--- app/Http/Controllers/Api/HomeDiaryController.php
+++ app/Http/Controllers/Api/HomeDiaryController.php
@@ -6,9 +6,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Model\Diary;
+use Illuminate\Support\Facades\Cache;
+
 
 class HomeDiaryController extends Controller
 {
+
+    private $cache_time = 20; // 缓存时间
 
     /**
      * 首页公开的日记
@@ -18,22 +22,26 @@ class HomeDiaryController extends Controller
     public function indexList(Diary $diary)
     {
         $page = request("page");
-        $par_page = request('par_page',5);
-        $diary = $diary->where([
-            'public' => 1,
-            'status' => 1,
-        ])->orderBy('created_at','desc')->paginate($par_page);
+        $lists = Cache::remember('HomeDiary_indexList' . $page, $this->cache_time, function () use ($diary) {
+            $par_page = request('par_page', 5);
+            $diary = $diary->where([
+                'public' => 1,
+                'status' => 1,
+            ])->orderBy('created_at', 'desc')->paginate($par_page);
 
-        $list = $diary->toArray();
-        foreach ($list['data']  as $key => &$v){
-            $v['avatar'] = $diary[$key]->userItem->avatar;
-            $v['username'] = $diary[$key]->userItem->username;
-        }
+            $list = $diary->toArray();
+            foreach ($list['data'] as $key => &$v) {
+                $v['created_at'] = tranTime(strtotime($v['created_at']));
+                $v['avatar'] = $diary[$key]->userItem->avatar;
+                $v['username'] = $diary[$key]->userItem->username;
+            }
 
-        $lists['lists'] = $list['data'];
-        $lists['total'] = ceil($list['total']/$par_page);
+            $lists['lists'] = $list['data'];
+            $lists['total'] = ceil($list['total'] / $par_page);
+            return $lists;
+        });
 
-        return api_success('',$lists);
+        return api_success('', $lists);
     }
 
 
@@ -41,10 +49,10 @@ class HomeDiaryController extends Controller
     {
         $r_id = request('r_id');
         if (empty($r_id)) return api_error('缺少参数！');
-        $diary = Diary::where('id',$r_id)->first();
+        $diary = Diary::where('id', $r_id)->first();
         $diary['username'] = $diary->userItem->username;
         $diary['avatar'] = $diary->userItem->avatar;
-        return  api_success('',$diary);
+        return api_success('', $diary);
     }
 
 }
